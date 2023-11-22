43bd861415b0504b8d44398944115b224be3bed7
lucasdifranco <lucasdifranco@gmail.com>
2023-11-22 11:20:33 -0300

    modified p-d

--- photo-processor.py
+++ photo-processor.py
@@ -1,10 +1,34 @@
 import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
-image_path = "./image/teste.jpg"
-img = cv.imread(image_path, cv.IMREAD_GRAYSCALE)
-kernel = 5
-blur = cv.GaussianBlur(img,(kernel,kernel),0)
-canny = cv.Canny(blur, 200, 300)
-cv.imshow("image",canny)
-cv.waitKey(0)
+
+class detlane():
+
+    def __init__(self) -> None:
+
+        self.image_path = "./image/teste.jpg"
+        self.kernel = 5
+        canny = self.canny_img(self.image_path)
+        pass
+
+    def canny_img(self,image_path) -> np.ndarray:
+        '''
+        Creates a canne-edge detection based on the original image.
+
+        Parameters:
+            image_path (path)
+            kernel (int)
+        Returns:
+            canny (ndarray)
+
+        '''
+        img = cv.imread(image_path, cv.IMREAD_GRAYSCALE)
+        
+        blur = cv.GaussianBlur(img,(self.kernel,self.kernel),0) # fix the blur of the image, you can change the kernel in __init__
+        canny = cv.Canny(blur, 200, 300) # you can change threshold 1 and 2 to fit your images
+
+        return canny
+    
+
+
+detlane()
